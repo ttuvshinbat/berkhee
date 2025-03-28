8356bf7fbe3fc548bd5af8c75cfda4a4c5b50e18
tuvshinbat t <tuvshinbat@gmail.com>
2025-03-28 16:17:47 +0800

    first comment

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Zugeer garj irsen huneer ni zairmag avhuul
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/berkhee.ochirbat"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Button
         </a>
+        <p>daraad uz</p>
       </header>
     </div>
   );
