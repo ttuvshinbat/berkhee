6158b67e58089ecdba8fff38c74d06524b97983b
tuvshinbat t <tuvshinbat@gmail.com>
2025-04-04 21:07:47 +0800

    hello

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Zugeer garj irsen huneer ni zairmag avhuul
-        </p>
-        <a
-          className="App-link"
-          href="https://www.facebook.com/berkhee.ochirbat"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Button
-        </a>
-        <p>daraad uz</p>
-      </header>
-    </div>
+
+
+  return (<div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Zugeer garj irsen huneer ni zairmag avhuul
+
+      </p>
+      <p>`sda neeree  bi chamd hairtai`</p>
+
+      <a
+        className="App-link"
+        href="https://www.facebook.com/berkhee.ochirbat"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Button
+      </a>
+      <p>daraad uz</p>
+    </header>
+  </div>
   );
 }
 
